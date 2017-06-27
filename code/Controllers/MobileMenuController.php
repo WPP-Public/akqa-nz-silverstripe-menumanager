58bb212354670f657bc94a63f8a2ccc4cd9e404a
Tartar, Clement <clement.tartar@heyday.co.nz>
2017-06-27 17:53:23 +1200

    Updated the menu manager controller to be able to have a JSON for mobile and desk mode.

--- code/Controllers/MobileMenuController.php
+++ code/Controllers/MobileMenuController.php
@@ -3,46 +3,157 @@
 /**
  * Class MobileMenuController
  */
-class MobileMenuController extends ContentController
+class MobileMenuController extends Controller
 {
     /**
-     * @var array
+     * @var string
      */
-    private static $allowed_actions = [
-        'systemsresults'
-    ];
+    public static $urlSegment = 'menuset';
 
+    /**
+     * @return mixed
+     */
+    public function index()
+    {
+        $response = $this->getResponse();
+
+        $response->addHeader('Content-Type', 'application/javascript');
+
+        if ($this->cacheInclude = Injector::inst()->get('CacheInclude')) {
+            $that = $this;
+            $name = 'JSONMenu';
+
+            return $this->cacheInclude->process(
+                $name,
+                function () use ($that, $name) {
+                    return $that->$name();
+                },
+                Injector::inst()->get('CacheIncludeKeyCreator')
+            );
+        }
+    }
+
+    /**
+     * return the JSON for the 2 different cases (Menu Manager or Children)
+     */
+    public function JSONMenu()
+    {
+        $menuMobileArray = array();
+
+        $request = $this->getRequest();
+
+        if ($this->getRequest()->getVar('level')) {
+            $level = $request->getVar('level');
+        } else {
+            $level = 0;
+        }
+
+        if ($this->getRequest()->getVar('mobile')) {
+            $mobile = $request->getVar('mobile');
+        } else {
+            $mobile = false;
+        }
+
+        // if using the menu manager for the menu, get items from there if not, using the children
+        if ($this->isUsingMenuManager()) {
+
+            if ($mobile) {
+                $menuSets = $this->getMenuSets();
+            } else {
+                $menuSets = MenuSet::get();
+            }
+
+            /** @var MenuSet $menuSet */
+            foreach ($menuSets as $menuSet) {
+
+
+                $menuItems = $menuSet->Children();
+
+                /** @var MenuItem $menuItem */
+                foreach ($menuItems as $menuItem) {
+
+                    // Recursive function to grab all the children
+                    $children = $this->recursiveChildren($menuItem->Page(), $level);
+
+                    $menuMobileArray[$menuSet->Name][] = [
+                        'title' => $menuItem->MenuTitle ?: '',
+                        'url' => $menuItem->Link ?: '',
+                        'children' => $children
+                    ];
+
+                }
+            }
+
+        } else {
+            // Using the normal Children from the CMS, not using the Mega Menu
+            $rootPages = SiteTree::get()
+                ->filter([
+                    'ParentID' => 0,
+                    'ShowInMenus' => 1
+                ]);
+
+            foreach ($rootPages as $rootPage) {
+
+                $children = $this->recursiveChildren($rootPage, $level);
+
+                $menuMobileArray['NormalMenu'][] = [
+                    'title' => $rootPage->MenuTitle ?: '',
+                    'url' => $rootPage->Link() ?: '',
+                    'children' => $children
+                ];
+            }
+        }
+        return json_encode($menuMobileArray);
+    }
+
+    /**
+     *  Recursive function to find all the children until the end
+     *
+     * @param $kids
+     * @return array
+     */
+    protected function recursiveChildren($kids, $level = 0)
+    {
+        $children = [];
+        $level--;
+
+        if ($greatKids = $kids->Children()) {
+            if ($level > 0){
+                foreach ($greatKids as $greatKid) {
+                    $children[] = [
+                        'title' => $greatKid->MenuTitle ?: '',
+                        'url' => $greatKid->Link() ?: '',
+                        'children' => $level > 0 ? $this->recursiveChildren($greatKid, $level) : []
+                    ];
+                }
+            }
+        }
+
+        return $children;
+    }
+
+    /**
+     * Check if it's using Menu manager or the normal children
+     * @return mixed
+     */
     public function isUsingMenuManager()
     {
         $config = SiteConfig::current_site_config();
         return $config->MenuManagerOption;
     }
 
-    public function getJSONMobileMenu()
-    {
-        // if using the menu manager for the menu, get items from there if not, using the children
-        if (isUsingMenuManager) {
-
-            $menuSets = $this->getMenuSets();
-
-            /** @var MenuSet $menuSet */
-            foreach ($menuSets as $menuSet) {
-
-                $menuItems = $menuSet->Children();
-            }
-
-            $menuMobileArray = $this->getArrayMobileMenu($menuItems);
-
-        } else {
-
-        }
-
-        json_encode($menuMobileArray);
-    }
-
+    /**
+     * Get the MenuSets defined in settings.
+     * @return mixed
+     */
     public function getMenuSets()
     {
         $config = SiteConfig::current_site_config();
         return $config->MenuSets();
+    }
+
+    public function getPageChildren($menuItem)
+    {
+
     }
 }
